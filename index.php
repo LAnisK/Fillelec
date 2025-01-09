bb8306994a73a02591e0677c15049463be19b141
Vald1906 <Valentin19.schmitt@gmail.com>
2025-01-09 12:58:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -35,71 +35,8 @@ if (isset($_GET['page']) && $_GET['page'] == 6) {
 <head>
     <meta charset="UTF-8">
     <title>Fillelec</title>
-    <link rel="stylesheet" href="assets/css/home.css">
 </head>
 <body>
-    <nav>
-        <center>
-        <br>
-        <h1>Bienvenue sur Fillelec</h1>
-
-        <div class="link-container">
-            <a href="index.php?page=1">Accueil</a>
-            <span></span>
-        </div>
-        <div class="link-container">
-            <a href="index.php?page=2">À propos</a>
-            <span></span>
-        </div>
-        <div class="link-container">
-            <a href="index.php?page=3">Contact</a>
-            <span></span>
-        </div>
-        <?php if (isset($_SESSION['email'])) : ?>
-            <div class="link-container">
-                <a href="index.php?page=4">Tableau de bord</a>
-                <span></span>
-            </div>
-            <div class="link-container">
-                <a href="index.php?page=6">Déconnexion</a>
-                <span></span>
-            </div>
-        <?php else : ?>
-            <div class="link-container">
-                <a href="index.php?page=5">Connexion</a>
-                <span></span>
-            </div>
-        <?php endif; ?>
-    </center>
-    </nav>
-
-    <div class="carrousel-container">
-        <div class="carrousel-images">
-            <img src="img\meca-distribution-vente-piece-detachee-automobile-grasse-realisations01.webp" alt="Image 1">
-            <img src="img\pieces-auto-vigneux.jpg" alt="Image 2">
-            <img src="img\Vente-de-pieces-detachees-carrosserie-et-mecanique-pour-voiture.jpeg" alt="Image 3">
-        </div>
-        <button class="prev">&#10094;</button>
-        <button class="next">&#10095;</button>
-    </div>
-
-    <div class="dots">
-        <span class="dot"></span>
-        <span class="dot"></span>
-        <span class="dot"></span>
-    </div>
-
-    <center>
-    <div class="link-container">
-    <a href="index.php?page=2">Offre du moment</a>
-    <span></span>
-    </center>
-
-    <br>
-    <br>
-    <br>
-    <br>
-
     <main>
         <?php
         // Déterminer la page à afficher en fonction du paramètre 'page' dans l'URL
@@ -143,7 +80,6 @@ if (isset($_GET['page']) && $_GET['page'] == 6) {
         }
         ?>
     </main>
-    <script> src="assets\js\index.js" </script>
 </body>
 </html>
 
--- vue/vue_connexion.php
+++ vue/vue_connexion.php
@@ -5,11 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fillelec - Page de connexion</title>
     <link rel="stylesheet" href="assets/css/connexion.css">
+    <link rel="stylesheet" href="assets/css/nav_bar_connexion.css">
 </head>
 <body>
+    <div class="welcome-text">
+        <h1>Bienvenue sur Fillelec</h1>
+    </div>
+    <center>
+    <br>
+    <div>
+        <nav>
+        <div class="link-container"><a href="index.php?page=1">Accueil</a></div>
+        <div class="link-container"><a href="index.php?page=2">À propos</a></div>
+        <div class="link-container"><a href="index.php?page=3">Contact</a></div>
+        </nav>
+    </div>
+    
     <div class="login-container">
         <img src="https://via.placeholder.com/100" alt="Logo Fillelec" class="logo">
-        <h1>Se connecter à Fillelec</h1>
+        <h2>Se connecter à Fillelec</h2>
         <form action="#" method="post">
             <input type="text" class="input-field" placeholder="Nom d'utilisateur" required>
             <input type="password" class="input-field" placeholder="Mot de passe" required>
@@ -17,6 +31,7 @@
         </form>
         <a href="#" class="forgot-password">Mot de passe oublié ?</a>
     </div>
+    </center>
 </body>
 </html>
 
