a48486e66dd73f7b69bd4d1819eec133ab008238
Théo_rouable <151970960+The1209@users.noreply.github.com>
2025-01-09 22:26:25 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+
+
+require_once "vue/vue_insert_contact.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nom = htmlspecialchars($_POST['nom']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    
+    $model->saveContact($nom, $email, $message);
+
+    
+    header("Location: controleur/home.php");
+    exit(); 
+} else {
+   
+    require_once "controleur/contact.php";
+}
+
+
+
+
+?>
